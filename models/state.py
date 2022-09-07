7d5cba6f003d4d8cd34aa0301e8d0aeabfaa4661
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-07 11:05:58 -0500

    8. DBStorage - Place: in  PLD, consejos de alejo

--- models/state.py
+++ models/state.py
@@ -4,13 +4,18 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.city import City
+from os import getenv
 
 
-class State(BaseModel, Base):
+class State(BaseModel, Base if (getenv("HBNB_TYPE_STORAGE")=="db") else object):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state", cascade="delete")
+
+    else:
+        name = ""
 
     @property
     def cities(self):
