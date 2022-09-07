cf9652d96122fee5db6aa81cd884756111fb8949
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-07 10:31:28 -0500

    8. DBStorage - Place: in  PLD

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
@@ -9,3 +10,9 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship(
+        "Place",
+        cascade="all",
+        backref=backref("cities", cascade="all"),
+        passive_deletes=True
+    )
--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
@@ -17,3 +18,10 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    places = relationship(
+        "Place",
+        cascade="all,delete",
+        backref=backref("user", cascade="all,delete"),
+        passive_deletes=True,
+        single_parent=True
+    )
