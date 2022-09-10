c551ef1118ac67c07eab2a2639bbf9565eda2a9e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-09-10 17:16:40 -0500

    Sat Sep 10 17:16:40 -05 2022

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,12 +8,12 @@ sudo apt-get -y install nginx
 # Creating directories
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 # Creating html for testing
-echo "HELLO WORLD" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>" | sudo tee /data/web_static/releases/test/index.html
 # Creating symbilic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 # Grants permissions
 sudo chown -R ubuntu:ubuntu /data/
 # Creating alias for location in NGINX config
-sudo sed -i '48i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '48i \\tlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 # Restar service
 sudo service nginx start
